a411e994f12ca5da2f82d39f8f5186804ab3de10
Dominik Kotecki <dominikus1910@gmail.com>
2018-01-06 14:06:52 +0100

    add Types

---
+++ src/Sentiment/Types.fs
@@ -0,0 +1,26 @@
+module Sentiment.Types
+open Fable.Core
+
+type Emotion =
+    | VeryNegative = -2
+    | Negative = -1
+    | Neutral = 0
+    | Positive = 1
+    | VeryPositive = 2
+
+[<Pojo>]
+type Classify = { text : string }
+
+[<Pojo>]
+type Train = { value: string; category: Emotion; weight : int option }
+
+[<Pojo>]
+type ClassificationResult = { text: string; score: Map<Emotion, float> }
+
+[<Pojo>]
+type ClassificatorState = { categories: Map<Emotion, Map<string, int>> }
+
+type Msg =
+    | Classify of Classify
+    | Train of Train
+    | GetState
